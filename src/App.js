94d801dd3f5997e296d188bca9fda160c507617d
hathai25 <phamhuyhathai@gmail.com>
2022-11-11 11:19:57 +0700

    add rich text editor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { Button } from "antd";
-import PageLayout from "./layout"
+import React, { useState, useEffect, useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
-const App = () => (
-  <PageLayout>
-    <Button type="primary">Button</Button>
-  </PageLayout>
-)
-
-export default App;
+export default function App({initialValue = "sad"}) {
+  const editorRef = useRef(null);
+  const [dirty, setDirty] = useState(false);
+  useEffect(() => setDirty(false), [initialValue]);
+  const save = () => {
+    if (editorRef.current) {
+      const content = editorRef.current.getContent();
+      setDirty(false);
+      editorRef.current.setDirty(false);
+      // an application would save the editor content to the server here
+      console.log(content);
+    }
+  };
+  
+  return (
+    <div style={{padding: 100}}>
+      <Editor
+        initialValue={initialValue}
+        onInit={(evt, editor) => editorRef.current = editor}
+        onDirty={() => setDirty(true)}
+      />
+      <button onClick={save} disabled={!dirty}>Save</button>
+      {dirty && <p>You have unsaved content!</p>}
+    </div>
+  );
+}
